2cf71f163ee18c8379076c8126be97462fe6a616
kiie9017 <39763880+kelee98@users.noreply.github.com>
2021-05-31 17:17:51 +0900

    #7 파이어베이스 auth 구현(로그인페이지)

---
+++ firestores/db.js
@@ -0,0 +1,8 @@
+// firebase를 초기 세팅하는 코드를 넣은 파일
+import firebase from './firestores/firebase';
+
+const db = firebase.firestore();
+const settings = {timesstampsInSnapshots:true};
+db.settings(settings);
+
+export default db;
