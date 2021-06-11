3be729d6ba27b7e2f6b11ebbccb7b97475c4e1e3
xianeml <xianemml@gmail.com>
2021-06-11 17:01:03 +0900

    #21 공통 컴포넌트 폴더 생성

--- components/DetailFeed.jsx
+++ components/DetailFeed.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import Link from "next/link";
-import Avatar from "./Avatar";
-import Comment from "./Comment";
+import React from 'react';
+import Link from 'next/link';
+import Avatar from './common/Avatar';
+import Comment from './Comment';
 import {
   Paper,
   TextField,
@@ -15,49 +15,47 @@ import {
   IconButton,
   Typography,
   makeStyles,
-} from "@material-ui/core";
-import SendIcon from "@material-ui/icons/Send";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import ChatIcon from "@material-ui/icons/Chat";
+} from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ChatIcon from '@material-ui/icons/Chat';
 
 const useStyles = makeStyles(() => ({
   feed: {
-    width: "100%",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   root: {
-    marginTop: "20px",
-    maxWidth: "1000px",
+    marginTop: '20px',
+    maxWidth: '1000px',
   },
   media: {
     height: 0,
-    paddingTop: "56.25%", // 16:9
+    paddingTop: '56.25%', // 16:9
   },
   comment: {
-    width: "600px",
-    height: "75px",
+    width: '600px',
+    height: '75px',
   },
-  cheader:{
-    width: "600px",
-    height: "55px",
+  cheader: {
+    width: '600px',
+    height: '55px',
   },
   commentItem: {
-    marginTop: "10px",
+    marginTop: '10px',
   },
   commentSend: {
-    display: "flex",
-    flexDirection: "row",
+    display: 'flex',
+    flexDirection: 'row',
   },
- 
 }));
 
 export default function DetailFeed() {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
   const [liked, setLiked] = React.useState(false);
- 
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
@@ -78,12 +76,12 @@ export default function DetailFeed() {
   const handleSendClick = () => {
     const comment = {
       id: comments.length + 1,
-      username: "aeuna",
+      username: 'aeuna',
       comment: inputs.comment,
     };
     setComments([...comments, comment]);
     setInputs({
-      comment: "",
+      comment: '',
     });
   };
 
@@ -91,33 +89,32 @@ export default function DetailFeed() {
     <div className={classes.feed}>
       <Card className={classes.root}>
         <CardHeader
-           className={classes.cheader}
+          className={classes.cheader}
           avatar={<Avatar size={1} />}
-          title="aeuna"
-          subheader="May 14, 2020"
+          title='aeuna'
+          subheader='May 14, 2020'
         />
         <CardMedia
           className={classes.media}
-          image="/images/test.jpeg"
-          title="food"
+          image='/images/test.jpeg'
+          title='food'
         />
         <CardContent>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <Typography variant='body2' color='textSecondary' component='p'>
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the 1500s.... (데이터 들어오면 몇자 이내로 자를 예정!)
           </Typography>
-          <Link href="/">
-            <Typography variant="body1">더보기</Typography>
+          <Link href='/'>
+            <Typography variant='body1'>더보기</Typography>
           </Link>
         </CardContent>
         <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites" onClick={handleHeartClick}>
-            {liked ? <FavoriteIcon color="secondary" /> : <FavoriteIcon />}
+          <IconButton aria-label='add to favorites' onClick={handleHeartClick}>
+            {liked ? <FavoriteIcon color='secondary' /> : <FavoriteIcon />}
             <ChatIcon />
           </IconButton>
         </CardActions>
-       
       </Card>
     </div>
   );
--- components/ProfileUpdatePopup.jsx
+++ components/ProfileUpdatePopup.jsx
@@ -4,7 +4,7 @@ import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import Avatar from './Avatar';
+import Avatar from './common/Avatar';
 import { Divider, Grid, Link } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
