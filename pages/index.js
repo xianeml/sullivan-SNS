8948b8a0b5ac2878f52db5bc2a729cf07879116d
xianeml <xianemml@gmail.com>
2021-06-13 12:48:18 +0900

    #8 사용자 photoUrl 관련 로그인 이슈 fix

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from "react";
-import Feed from "../components/Feed";
-import db from "../firestores/db";
+import React, { useEffect, useState } from 'react';
+import Feed from '../components/Feed';
+import db from '../firestores/db';
 
 const index = () => {
   const [comments, setComments] = useState([
     {
       id: 1,
-      username: "aeuna",
-      comment: "정말 맛있겠다!",
+      username: 'aeuna',
+      comment: '정말 맛있겠다!',
     },
     {
       id: 2,
-      username: "kelee98",
-      comment: "장소가 어디인가용?",
+      username: 'kelee98',
+      comment: '장소가 어디인가용?',
     },
     {
       id: 3,
-      username: "xianeml",
-      comment: "가고 싶어요!",
+      username: 'xianeml',
+      comment: '가고 싶어요!',
     },
     {
       id: 4,
-      username: "huiji0315",
-      comment: "나도 가고 싶어요!",
+      username: 'huiji0315',
+      comment: '나도 가고 싶어요!',
     },
   ]);
   const [feeds, setFeeds] = useState(null);
@@ -32,7 +32,7 @@ const index = () => {
     const fetchFeeds = async () => {
       const feedsData = []; //받아온 데이터를 저장할 배열
       await db
-        .collection("feed")
+        .collection('feed')
         .get() // "feed" 컬렉션의 모든 다큐먼트를 갖는 프로미스 반환
         .then((docs) => {
           docs.forEach((doc) => {
@@ -42,7 +42,7 @@ const index = () => {
               photoUrl: doc.data().photoUrl,
               author: {
                 displayName: doc.data().author.displayName,
-                photoURL: doc.data().author.photoURL,
+                photoUrl: doc.data().author.photoUrl,
                 uid: doc.data().author.uid,
               },
               location: doc.data().location,
