483b4b73addf3975c5a022105782354c700987d7
aeuna <4294psy@naver.com>
2021-07-04 17:32:53 +0900

    #38 마이피드 최신순으로 가져오도록 orderBy추가

--- pages/myfeed.js
+++ pages/myfeed.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { Divider, Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import Profile from '../components/myFeed/Profile';
-import PhotoGrid from '../components/myFeed/PhotoGrid';
-import db from '../firestores/db';
+import React, { useState, useEffect } from "react";
+import { Divider, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Profile from "../components/myFeed/Profile";
+import PhotoGrid from "../components/myFeed/PhotoGrid";
+import db from "../firestores/db";
 
 const useStyles = makeStyles((theme) => ({
   divider: {
     margin: theme.spacing(6, 0),
   },
   container: {
-    [theme.breakpoints.down('lg')]: {
+    [theme.breakpoints.down("lg")]: {
       padding: theme.spacing(0, 30),
     },
-    [theme.breakpoints.down('md')]: {
+    [theme.breakpoints.down("md")]: {
       padding: theme.spacing(0, 2),
     },
-    [theme.breakpoints.down('sm')]: {
+    [theme.breakpoints.down("sm")]: {
       padding: 0,
     },
   },
@@ -34,14 +34,14 @@ const myFeed = () => {
   }, []);
 
   const getUserInfo = async () => {
-    const userRef = db.collection('myuser').doc('SFCKJmd9KzCpO5H77wz1');
+    const userRef = db.collection("myuser").doc("SFCKJmd9KzCpO5H77wz1");
     const userDoc = await userRef.get();
     const userInfo = userDoc.data();
     setUser(userInfo);
 
     const myFeedList = [];
-    const feedRef = db.collection('feed');
-    const feedSnapshot = await feedRef.get();
+    const feedRef = db.collection("feed");
+    const feedSnapshot = await feedRef.orderBy("create_at", "desc").get();
     feedSnapshot.forEach((doc) => {
       if (doc.data().author.uid === userInfo.uid) {
         myFeedList.push(doc.data());
@@ -55,7 +55,7 @@ const myFeed = () => {
   return (
     <div className={classes.root}>
       <Profile user={user} feedList={feedList} />
-      <Divider variant='middle' light className={classes.divider} />
+      <Divider variant="middle" light className={classes.divider} />
       <Grid container spacing={3} className={classes.container}>
         {loading ? (
           <div>Loading...</div>
