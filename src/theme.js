2c552699f3809ff493d50be87ce82ca71bdf0459
aeuna <4294psy@naver.com>
2021-07-17 15:02:49 +0900

    #50 createTheme typeError 해결

--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,19 @@
-import { createTheme } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { createMuiTheme } from "@material-ui/core/styles";
+import { red } from "@material-ui/core/colors";
 
-const theme = createTheme({
+const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: "#556cd6",
     },
     secondary: {
-      main: '#707070',
+      main: "#707070",
     },
     error: {
       main: red.A400,
     },
     background: {
-      default: '#fff',
+      default: "#fff",
     },
   },
 });
